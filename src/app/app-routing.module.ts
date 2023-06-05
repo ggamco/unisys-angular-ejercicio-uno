82e7e282e87ef893e210f25a8a755b97b80a7cd4
Gustavo Gamboa <gustavo.gamboa@allianzdirect.es>
2023-06-05 17:53:57 +0200

    init commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { VistaPrincipalComponent } from './vista-principal/vista-principal.component';
+import { VistaDetalleComponent } from './vista-detalle/vista-detalle.component';
+import { PrincipalComponent } from './view/principal/principal.component';
+import { AboutComponent } from './view/about/about.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "inicio", component: VistaPrincipalComponent },
+  { path: "portada", component: VistaDetalleComponent },
+  { path: "blog", component: PrincipalComponent },
+  { path: "contacto", component: AboutComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { VistaPrincipalComponent } from './vista-principal/vista-principal.component';
+import { VistaDetalleComponent } from './vista-detalle/vista-detalle.component';
+import { PrincipalComponent } from './view/principal/principal.component';
+import { AboutComponent } from './view/about/about.component';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VistaPrincipalComponent,
+    VistaDetalleComponent,
+    PrincipalComponent,
+    AboutComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
